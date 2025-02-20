8ad7205c9643f0970d298e462c412360d57321be
sitimfthnh <sitimuafthonah832@gmail.com>
2025-02-20 13:57:45 +0700

    praktikum01

--- README.md
+++
@@ -1,7 +0,0 @@
-# pemprograman web 2
-
-## repository praktikum pemprograman web 2
-
-- Nama : Siti Mufathonah
-- NIM : 0110124110
-- Rombel : SI 11
---
+++ variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinisikan variabel
+$nama = 'Siti';
+$umur = 19;
+$berat = 44.05;
+
+echo 'nama saya '. $nama;
+echo '<br/>Umur: '. $umur. 'Tahun';
+echo '<br/>Berat : '. $berat;
+
+?>
