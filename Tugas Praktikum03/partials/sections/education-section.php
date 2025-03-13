6f23ed742078c686de53363e3900d18a78941c0c
sitimfthnh <sitimuafthonah832@gmail.com>
2025-03-13 10:38:01 +0700

    praktikum04

--- Tugas Praktikum03/partials/sections/education-section.php
+++
@@ -1,8 +0,0 @@
-<section>
-                            <h2 class="text-secondary fw-bolder mb-4">Education</h2>
-                            <!-- Education Card 1-->
-                           <?php include_once "partials/cards/education-card-1.php"?>
-                            <!-- Education Card 2-->
-                            <?php include_once "partials/cards/education-card-2.php"?> 
-</section>
-
